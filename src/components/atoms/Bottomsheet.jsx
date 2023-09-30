94e2f95ec9168864b3a609fc54266a4e1f92912f
juyeongnoh <juyeongnoh@gmail.com>
2023-10-01 01:02:53 +0900

    design: Bottomsheet className 매개변수 추가

--- src/components/atoms/Bottomsheet.jsx
+++ src/components/atoms/Bottomsheet.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Button } from "./Button";
-import { Input } from "./Input";
 
-export const Bottomsheet = ({ children }) => {
+export const Bottomsheet = ({ className, children }) => {
   return (
-    <div className="relative w-96 h-96 p-4 rounded-t-xl bg-white text-black shadow-lg">
+    <div
+      className={`relative w-96 h-96 p-4 rounded-t-xl bg-white text-black shadow-lg ${className}`}>
       <div className="absolute w-9 h-1 bg-gray-400 rounded-full left-1/2 -translate-x-1/2 top-1.5"></div>
       {children}
       <div className="absolute bottom-6 left-0 w-96">

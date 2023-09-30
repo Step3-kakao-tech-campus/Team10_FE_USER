bdc159b266226121be073e49e541f8a5e1632c07
juyeongnoh <juyeongnoh@gmail.com>
2023-10-01 00:56:30 +0900

    design: Bottomsheet 안쓰는 매개변수, 컴포넌트 제거

--- src/components/atoms/Bottomsheet.jsx
+++ src/components/atoms/Bottomsheet.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Button } from "./Button";
+import { Input } from "./Input";
 
-export const Bottomsheet = ({ type, children }) => {
+export const Bottomsheet = ({ children }) => {
   return (
     <div className="relative w-96 h-96 p-4 rounded-t-xl bg-white text-black shadow-lg">
       <div className="absolute w-9 h-1 bg-gray-400 rounded-full left-1/2 -translate-x-1/2 top-1.5"></div>
-      <Button label="로그인" />
+      {children}
+      <div className="absolute bottom-6 left-0 w-96">
+        <Button label="로그인" />
+      </div>
     </div>
   );
 };

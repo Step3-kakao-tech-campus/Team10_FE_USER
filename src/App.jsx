9ef811b833008a67d47236a2914cf0f44cf0c387
juyeongnoh <juyeongnoh@gmail.com>
2023-10-07 23:04:11 +0900

    fix: conflict
    branch merge 하며 발생하는 작은 이슈들 해결
    - package.json 중복된 패키지 삭제
    - export default 형식의 컴포넌트, export const 형식의 컴포넌트 혼용으로 로드가 안되는 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { MainLayout } from "./layouts/MainLayout";
 import { ReservationListPage } from "./pages/ReservationListPage";
-import { ReservationPage } from "./pages/ReservationPage";
-import {CarwashDetailPage} from './pages/CarwashDetailPage';
-import {ReservationPage} from "./pages/ReservationPage";
+import ReservationPage from "./pages/ReservationPage";
+import CarwashDetailPage from "./pages/CarwashDetailPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/reservation" element={<ReservationPage />}></Route>
           <Route
             path="/reservationlist"
             element={<ReservationListPage />}></Route>
-            <Route path="/carwashdetail" element={<CarwashDetailPage/>}></Route>
+          <Route path="/carwashdetail" element={<CarwashDetailPage />}></Route>
           <Route path="/reservation" element={<ReservationPage />}></Route>
         </Route>
       </Routes>
--- src/pages/CarwashDetailPage.jsx
+++ src/pages/CarwashDetailPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import CarwashDetailTemplate from '../components/templates/CarwashDetailTemplate';
+import React from "react";
+import CarwashDetailTemplate from "../components/templates/CarwashDetailTemplate";
 const CarwashDetailPage = () => {
-    return (
-        <div className=" ">
-            <CarwashDetailTemplate/>
-        </div>
-    );
+  return (
+    <div className=" ">
+      <CarwashDetailTemplate />
+    </div>
+  );
 };
 
 export default CarwashDetailPage;

9b0dd8ef8cda9bb718dc3ffcba7d6f5bc52ac3dd
juyeongnoh <juyeongnoh@gmail.com>
2023-10-12 11:03:39 +0900

    refactor: DurationPicker 상태 끌어올리기 용도 콜백 매개변수 추가

--- src/components/molecules/DurationPicker.jsx
+++ src/components/molecules/DurationPicker.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 
-const DurationPicker = ({ selectedStartTime, setSelectedStartTime }) => {
+const DurationPicker = ({ handleButtonClick }) => {
   const [selectedDuration, setSelectedDuration] = useState(30);
-  const durations = [30, 60, 90, 120, 180, 240]; 
+  const durations = [30, 60, 90, 120, 180, 240];
 
   const handleDurationClick = (duration) => {
     setSelectedDuration(duration);
+    handleButtonClick(duration);
   };
 
   return (
     <div>
-      <div className="my-2 flex flex-items justify-center gap-4">
+      <div className="flex gap-4">
         {durations.map((duration) => (
           <button
             key={duration}
             onClick={() => handleDurationClick(duration)}
             className={`p-2 border ${
-              selectedDuration === duration ? "bg-sky-500 text-white" : "bg-white"
-            } rounded-md cursor-pointer`}
-          >
+              selectedDuration === duration
+                ? "bg-sky-500 text-white"
+                : "bg-white"
+            } rounded-md cursor-pointer`}>
             {duration}분
           </button>
         ))}
       </div>
-      <div className="ml-2">
-          선택한 사용 시간: {selectedDuration}분
-      </div>
+      <div>선택한 사용 시간: {selectedDuration}분</div>
     </div>
   );
 };

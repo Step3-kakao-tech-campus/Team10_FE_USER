8f10fa47c90ff01d0344e2da0540b3cb85a74ed8
juyeongnoh <juyeongnoh@gmail.com>
2023-10-12 11:09:44 +0900

    design: ReviewPostTemplate 마진 값 제거 후 gap으로 간격 조정

--- src/components/templates/ReviewPostTemplate.jsx
+++ src/components/templates/ReviewPostTemplate.jsx
@@ -4,20 +4,15 @@ import { Button } from "../atoms/Button";
 
 const ReviewPostTemplate = () => {
   return (
-    <div className="flex flex-col gap-7">
-      <h1 className="text-2xl mx-4 mt-20 font-bold">
-        예약한 세차장이 어땠나요?
-      </h1>
+    <div className="relative grid gap-6">
+      <h1 className="text-2xl font-bold">예약한 세차장이 어땠나요?</h1>
       <StarPicker />
       <BadgeSet />
-      <div className="flex justify-center items-center">
-        <textarea
-          className="w-96 h-64 mt-4 rounded-lg justify-center items-center bg-slate-100"
-          placeholder="후기를 입력해주세요"
-        />
-      </div>
-
-      <Button label="후기등록" className="mt-10" />
+      <textarea
+        className="h-40 p-4 rounded-lg items-center bg-slate-100 resize-none"
+        placeholder="후기를 입력해주세요."
+      />
+      <Button label="후기등록" className="fixed bottom-0" />
     </div>
   );
 };

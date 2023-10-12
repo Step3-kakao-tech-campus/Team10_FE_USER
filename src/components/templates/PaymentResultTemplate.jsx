47a8758530a687587da6774528512aff4825deff
juyeongnoh <juyeongnoh@gmail.com>
2023-10-12 11:08:38 +0900

    design: PaymentResultTemplate 마진 값 제거 후 gap으로 간격 조정

--- src/components/templates/PaymentResultTemplate.jsx
+++ src/components/templates/PaymentResultTemplate.jsx
@@ -3,46 +3,30 @@ import { MapWithPin } from "../atoms/MapWithPin";
 import { Button } from "../atoms/Button";
 
 const iconsrc = {
-  calendar: "TextWithIcon\\calendar.png",
-  clock: "TextWithIcon\\clock.png",
-  location: "TextWithIcon\\location.png",
-  price: "TextWithIcon\\price.png",
+  calendar: "/TextWithIcon/calendar.png",
+  clock: "TextWithIcon/clock.png",
+  location: "TextWithIcon/location.png",
+  price: "TextWithIcon/price.png",
 };
 
 const PaymentResultTemplate = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-start gap-3">
-      <div className="mx-4 mt-20 text-2xl font-bold ">
-        결제가 완료되었습니다!
-      </div>
-      <div className="mx-4 text-left text-sm">득광하세요!</div>
-      <TextWithIcon
-        className="mx-4 mt-4"
-        text="2023/09/08"
-        iconsrc={iconsrc.calendar}
-      />
-
-      <div className="mx-4 w-96 h-[0px] border border-neutral-200"></div>
-      <TextWithIcon
-        className="mx-4"
-        text="13:00 ~ 15:00"
-        iconsrc={iconsrc.clock}
-      />
-      <div className="mx-4 w-96 h-[0px] border border-neutral-200"></div>
-      <TextWithIcon
-        className="mx-4"
-        text="용봉세차타운:베이 7"
-        iconsrc={iconsrc.location}
-      />
-      <div className="mx-4 w-96 h-[0px] border border-neutral-200"></div>
-      <TextWithIcon className="mx-4" text="20,000원" iconsrc={iconsrc.price} />
-      <MapWithPin
-        className="mx-4 mt-4"
-        lat="35.17388"
-        lng="126.9112"
-        text="용봉세차타운"
-      />
-      <Button label="홈으로" />
+    <div className="relative grid gap-8">
+      <section className="grid gap-4">
+        <div className="text-2xl font-bold ">결제가 완료되었습니다!</div>
+        <div className="text-left text-sm">득광하세요!</div>
+      </section>
+      <section className="grid gap-4">
+        <TextWithIcon text="2023/09/08" iconsrc={iconsrc.calendar} />
+        <hr />
+        <TextWithIcon text="13:00 ~ 15:00" iconsrc={iconsrc.clock} />
+        <hr />
+        <TextWithIcon text="용봉세차타운:베이 7" iconsrc={iconsrc.location} />
+        <hr />
+        <TextWithIcon text="20,000원" iconsrc={iconsrc.price} />
+        <MapWithPin lat="35.17388" lng="126.9112" text="용봉세차타운" />
+      </section>
+      <Button className="fixed bottom-0" label="홈으로" />
     </div>
   );
 };

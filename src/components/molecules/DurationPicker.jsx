760c297cd833f7c7515a43fece4c2aab281df704
minjuko <200778@jnu.ac.kr>
2023-10-13 14:22:00 +0900

    design: 사용시간 CSS

--- src/components/molecules/DurationPicker.jsx
+++ src/components/molecules/DurationPicker.jsx
@@ -11,7 +11,7 @@ const DurationPicker = ({ handleButtonClick }) => {
 
   return (
     <div>
-      <div className="flex gap-2">
+      <div className="flex flex-wrap gap-2">
         {durations.map((duration) => (
           <button
             key={duration}

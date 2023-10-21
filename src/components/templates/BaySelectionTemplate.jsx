f9569f5f83d5f2ebea50a1b15f41c90df9ec7a62
catnofat <catnofat@gmail.com>
2023-10-21 18:09:14 +0900

    fix: BaySelectionTemplate @tanstack reactquery로 변경

--- src/components/templates/BaySelectionTemplate.jsx
+++ src/components/templates/BaySelectionTemplate.jsx
@@ -4,7 +4,7 @@ import Image from "../atoms/Image";
 import BayList from "../molecules/BayList";
 import { useEffect, useState } from "react";
 import { carwashes_bays } from "../../apis/carwashes";
-import { useQuery } from "react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 const BaySelectionTemplate = ({}) => {
   const name = "포세이돈워시 용봉점";
   const openingHours = {
@@ -13,12 +13,15 @@ const BaySelectionTemplate = ({}) => {
   };
   const [bayList, setBayList] = useState([]);
 
-  const { isLoading, data } = useQuery("baylists", carwashes_bays, {
-    onSuccess: (data) => {
-      setBayList(data.data.response.bayList);
-    },
+  const { data } = useSuspenseQuery({
+    queryKey: ["baylists"],
+    queryFn: carwashes_bays,
   });
-
+  useEffect(() => {
+    if (data) {
+      setBayList(data.data.response.bayList);
+    }
+  }, []);
   // 세차장별 예약 내역 조회 '/carwashes/{carwash_id}/bays'
   return (
     <div className="relative p-4">
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { worker } from "./mocks/worker";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 if (process.env.NODE_ENV === "development") {
   worker.start();
 }

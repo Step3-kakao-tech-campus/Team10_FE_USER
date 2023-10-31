7f48e959f3a3d7808fc232efc59604d5a188bac4
minjuko <200778@jnu.ac.kr>
2023-10-31 21:44:04 +0900

    feat: reducer 작성

---
+++ src/store/action.js
@@ -0,0 +1,26 @@
+// 액션 타입 정의
+export const SET_CARWASH_ID = "SET_CARWASH_ID";
+export const SET_BAY_ID = "SET_BAY_ID";
+export const SAVE_RESERVATION = "SAVE_RESERVATION";
+export const RESET_STORE = "RESET_STORE";
+
+// 액션 생성자 정의
+
+export const setCarwashId = (carwashId) => ({
+  type: SET_CARWASH_ID,
+  payload: carwashId,
+});
+
+export const setBayId = (bayId) => ({
+  type: SET_BAY_ID,
+  payload: bayId,
+});
+
+export const saveReservation = (startTime, endTime) => ({
+  type: SAVE_RESERVATION,
+  payload: { startTime, endTime },
+});
+
+export const resetStore = () => ({
+  type: RESET_STORE,
+});

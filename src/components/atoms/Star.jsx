f97f77f17176038ef6f6e1e2f9bfb594567ca339
minjuko <200778@jnu.ac.kr>
2023-11-01 14:54:18 +0900

    fix: Star 컴포넌트 총 리뷰  건수 출력

--- src/components/atoms/Star.jsx
+++ src/components/atoms/Star.jsx
@@ -5,11 +5,11 @@ import StarImg from "/StarPicker/checkedstar.svg";
  * @param starcount : 별점, reviewcount : 리뷰수 받아옴
  * @returns 별점 컴포넌트
  */
-const Star = ({ starcount, reviewCount }) => {
+const Star = ({ starCount, reviewCount }) => {
   return (
     <div className="flex items-center gap-1">
       <img className="w-3 h-3" src={StarImg} alt="별 아이콘" />
-      <div className="text-sm text-black">{starcount}</div>
+      <div className="text-sm text-black">{starCount}</div>
       <div className="text-sm text-neutral-500">({reviewCount})</div>
     </div>
   );

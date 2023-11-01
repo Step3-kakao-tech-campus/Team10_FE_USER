5a2c9550c683047bd2a43aa9f3766b8b86a3216e
minjuko <200778@jnu.ac.kr>
2023-11-01 21:02:35 +0900

    design: 상태바 css

--- src/components/atoms/StatusBar.jsx
+++ src/components/atoms/StatusBar.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import BackImg from "/GNB/Back.svg";
+import { useDispatch } from "react-redux";
+import { resetStore } from "../../store/action";
 
 const StatusBar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const token = localStorage.getItem("token");
 
   const handleLogOut = () => {
     if (token) {
       alert("로그아웃 성공");
       localStorage.removeItem("token");
+      dispatch(resetStore());
       window.location.reload();
     }
   };
@@ -18,16 +22,13 @@ const StatusBar = () => {
   const isMainPage = location.pathname === "/";
 
   return (
-    <header className="w-full border-b-2 border-gray-300">
-      <Link to="/" className="flex items-center px-4 py-3">
-        {!isMainPage && (
-          <img
-            src={BackImg}
-            alt="뒤로가기"
-            className="h-10 w-30"
-            onClick={() => navigate(-1)}
-          />
-        )}
+    <header className="w-full border-b-2 border-gray-300 h-14">
+      <Link to="/" className="flex items-center p-4">
+        <div>
+          {!isMainPage && (
+            <img src={BackImg} alt="뒤로가기" onClick={() => navigate(-1)} />
+          )}
+        </div>
         <div className="flex items-center ml-auto space-x-4">
           {token ? (
             <Link

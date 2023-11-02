23fde2a0013d970d0fba0db33469bffa30bbd85e
minjuko <200778@jnu.ac.kr>
2023-11-02 14:41:03 +0900

    design: 예약 취소 버튼

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = ({ variant, className, children, ...props }) => {
   const styles = {
-    long: "block w-full h-14 p-4 bg-primary text-white font-semibold rounded-none",
+    long: "block w-full h-14 p-4 bg-primary text-white font-semibold",
     small: "block w-28 h-14 bg-sky-100 text-sky-500 font-semibold rounded-xl",
     home: "relative flex items-start w-full h-20 bg-white p-4 text-left break-keep overflow-hidden shadow-xl rounded-xl",
     cancel: "w-16 h-12 px-4 bg-red-500 text-white text-sm  rounded-md",

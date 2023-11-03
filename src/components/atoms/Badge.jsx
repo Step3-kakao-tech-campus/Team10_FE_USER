eee663efe16b7ccfaccccb4446a5ee2405b12eab
minjuko <200778@jnu.ac.kr>
2023-11-03 20:03:16 +0900

    design: 리뷰 페이지 스타일 수정

--- src/components/atoms/Badge.jsx
+++ src/components/atoms/Badge.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 const getType = (type) => {
   switch (type) {
     case "unclicked":
-      return "flex items-center align-middle w-auto h-6 px-3.5 py-1 leading-normal text-center text-xs font-normal text-black bg-slate-200 rounded-full border border-slate-300";
+      return "flex items-center align-middle w-auto h-6 px-3.5 py-1 leading-normal text-center text-xs font-normal text-black bg-gray-100 rounded-full border border-slate-300";
     case "onclicked":
-      return "flex items-center align-middle w-auto h-6 px-3.5 py-1 leading-normal text-center text-xs font-normal text-white bg-blue-500 rounded-full";
+      return "flex items-center align-middle w-auto h-6 px-3.5 py-1 leading-normal text-center text-xs font-normal text-white bg-primary rounded-full";
     default:
       return "";
   }

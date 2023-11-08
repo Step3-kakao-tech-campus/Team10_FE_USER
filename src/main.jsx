e1785fe63173296a8ba6e4de00bb19502e1a4d40
minjuko <200778@jnu.ac.kr>
2023-11-08 19:33:11 +0900

    chore: main 충돌 해결

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { worker } from "./mocks/worker";
+//import { worker } from "./mocks/worker";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store";
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
-const queryClient = new QueryClient();
+
+// if (process.env.NODE_ENV === "development") {
+//   worker.start();
+// }
+const queryClient = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

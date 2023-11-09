652b1788faea2631263e48e75dfc68aad3e02746
minjuko <200778@jnu.ac.kr>
2023-11-09 22:37:30 +0900

    chore: 키포인트 텍스트

--- src/components/atoms/KeyPointInfo.jsx
+++ src/components/atoms/KeyPointInfo.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 const KeyPointInfo = ({ selectedPoints }) => {
   const keypointToText = {
-    1: "하부세차",
-    2: "개러지형 독립공간",
-    3: "야간조명",
-    4: "100% 수돗물",
-    5: "휴게실",
-    6: "에어컨",
-    7: "발수코팅건",
+    8: "하부세차",
+    9: "개러지형 독립공간",
+    10: "야간조명",
+    11: "100% 수돗물",
+    12: "휴게실",
+    13: "에어컨",
+    14: "발수코팅건",
   };
-
-  // 확장> 키포인트 텍스트에 아이콘까지 매핑
 
   return (
     <div className="grid gap-2 p-4 bg-gray-100 rounded-lg">

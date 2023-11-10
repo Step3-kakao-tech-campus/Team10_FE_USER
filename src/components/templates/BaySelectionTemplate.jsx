d4ff3245b219a2915096f03410cce02be2a136fd
juyeongnoh <juyeongnoh@gmail.com>
2023-11-10 15:21:31 +0900

    design: 베이 선택 화면 UI 수정

--- src/components/templates/BaySelectionTemplate.jsx
+++ src/components/templates/BaySelectionTemplate.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TimeImage from "/StoreInfo/Time.svg";
 import Image from "../atoms/Image";
 import BayList from "../organisms/BayList";
 import { carwashesBays, carwashesInfo } from "../../apis/carwashes";
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQueries } from "@tanstack/react-query";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -11,24 +11,22 @@ const BaySelectionTemplate = ({ carwashId }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { data: carwashInfoData } = useSuspenseQuery({
-    queryKey: ["getCarwashesInfo", carwashId],
-    queryFn: () => carwashesInfo(carwashId),
+  const [carwashInfoData, bayListData] = useSuspenseQueries({
+    queries: [
+      {
+        queryKey: ["getCarwashesInfo", carwashId],
+        queryFn: () => carwashesInfo(carwashId),
+      },
+      {
+        queryKey: ["baylists", carwashId],
+        queryFn: () => carwashesBays(carwashId),
+      },
+    ],
   });
 
-  const { data: bayListData } = useQuery({
-    queryKey: ["baylists", carwashId],
-    queryFn: () => carwashesBays(carwashId),
-    enabled: !!carwashId,
-  });
-
-  if (!carwashInfoData || !bayListData) {
-    return <div>Loading...</div>;
-  }
-
-  const detailData = carwashInfoData.data.response.optime;
-  const name = carwashInfoData.data.response.name;
-  const bayList = bayListData.data.response.bayList;
+  const detailData = carwashInfoData.data.data.response.optime;
+  const name = carwashInfoData.data.data.response.name;
+  const bayList = bayListData.data.data.response.bayList;
 
   const handleBayClick = (bayId) => {
     dispatch({ type: "SET_BAY_ID", payload: bayId });
@@ -36,10 +34,10 @@ const BaySelectionTemplate = ({ carwashId }) => {
   };
 
   return (
-    <div className="relative p-4">
-      <div className="mb-4 text-xl font-bold">{name}: 베이 선택</div>
-      <div className="flex gap-2 mb-4">
-        <Image src={TimeImage} alt="영업시간" className="py-1" />
+    <div className="p-4 grid-4">
+      <div className="text-xl font-bold">{name}: 베이 선택</div>
+      <div className="flex items-center gap-2 mb-4">
+        <Image src={TimeImage} alt="영업시간" className="" />
         <div>
           <div>
             평일 {detailData.weekday.start} ~ {detailData.weekday.end}

e6fddabd54a8f4e2f675c2c66a412d3fbe00d368
minjuko <200778@jnu.ac.kr>
2023-11-10 23:48:23 +0900

    fix: 로그인 에러 상태코드에 따른 처리

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,4 @@
 import LoginForm from "../components/organisms/LoginForm";
-import { GeneralErrorBoundary } from "../components/atoms/GeneralErrorBoundary";
-import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   return <LoginForm />;

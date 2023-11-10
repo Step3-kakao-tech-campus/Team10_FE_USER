9ef9b418efa8567ad32284da3dc85f4839089a05
minjuko <200778@jnu.ac.kr>
2023-11-11 02:07:48 +0900

    chore: 잘못된 코드 수정

--- src/components/atoms/MapWithPin.jsx
+++ src/components/atoms/MapWithPin.jsx
@@ -1,10 +1,6 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
-const KakaoMap = ({
-  currentloc = { latitude: 33.450701, longitude: 126.570667 },
-  className,
-  mapdata = [],
-}) => {
+const MapWithPin = ({ lat, lng, text, className }) => {
   useEffect(() => {
     const script = document.createElement("script");
     script.async = true;
@@ -15,64 +11,52 @@ const KakaoMap = ({
     document.head.appendChild(script);
 
     script.onload = () => {
-      // Ensure the global kakao variable is on the window object
-      const kakao = window.kakao;
-
       kakao.maps.load(() => {
         const container = document.getElementById("map");
         const options = {
-          center: new kakao.maps.LatLng(
-            currentloc.latitude - 0.03,
-            currentloc.longitude
-          ),
-          level: 7,
+          center: new kakao.maps.LatLng(lat, lng),
+          level: 3,
         };
-
         const map = new kakao.maps.Map(container, options);
+        const position = new kakao.maps.LatLng(lat, lng);
 
-        const markerImage = new kakao.maps.MarkerImage(
-          "/myloca.png",
-          new kakao.maps.Size(20, 30),
-          { offset: new kakao.maps.Point(10, 25) }
-        );
+        const iwContent = `
+          <div style="
+            position: relative;
+            padding: 5px 10px;
+            background: #0098FF;
+            border-radius: 4px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            font-size: 12px;
+            color: #FFFFFF;
+            text-align: center;
+          ">
+            ${text}
+            <div style="
+              position: absolute;
+              left: 50%;
+              bottom: -8px; 
+              margin-left: -8px; 
+              width: 0;
+              height: 0;
+              border-left: 8px solid transparent;
+              border-right: 8px solid transparent;
+              border-top: 8px solid #0098FF;
+            "></div>
+          </div>
+        `;
 
-        const marker = new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(
-            currentloc.latitude,
-            currentloc.longitude
-          ),
-          image: markerImage,
-        });
-
-        marker.setMap(map);
-
-        mapdata.forEach((el) => {
-          const position = new kakao.maps.LatLng(
-            el.location.latitude,
-            el.location.longitude
-          );
-          const iwContent = `<div style="position: relative; bottom: -16px; display: inline-block; padding: 5px 10px; background: #0098FF; border-radius: 8px; font-size: 12px; color: #FFFFFF; text-align: center;">
-            ${el?.name}
-            <div style="position: absolute; left: 50%; bottom: -8px; margin-left: -8px; width: 0; height: 0; border-left: 8px solid transparent; border-right: 8px solid transparent; border-top: 10px solid #0098FF;"></div>
-          </div>`; // Inline styles can be kept as is or moved to CSS classes
-
-          new kakao.maps.CustomOverlay({
-            map: map,
-            position: position,
-            content: iwContent,
-            yAnchor: 1,
-          });
+        const customOverlay = new kakao.maps.CustomOverlay({
+          map: map,
+          position: position,
+          content: iwContent,
+          yAnchor: 1,
         });
       });
     };
+  }, [lat, lng, text]);
 
-    // Remove the script from the document when the component unmounts
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, [currentloc, mapdata]); // Dependency array to re-run the effect when currentloc or mapdata changes
-
-  return <div id="map" className={`${className}`}></div>;
+  return <div id="map" className={`h-72 rounded-3xl ${className}`}></div>;
 };
 
-export default KakaoMap;
+export default MapWithPin;

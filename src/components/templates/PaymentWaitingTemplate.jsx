af0ac17893383accd7477d7f3112a34d1e04e3b3
Jwahoon <catnofat@gmail.com>
2023-11-11 16:38:20 +0900

    fix: paymentwaitingpage 에러 관리 코드 추가

--- src/components/templates/PaymentWaitingTemplate.jsx
+++ src/components/templates/PaymentWaitingTemplate.jsx
@@ -1,11 +1,16 @@
 import { Button } from "../atoms/Button";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import { pgapprove } from "../../apis/payment";
+import CustomModal from "../atoms/CustomModal";
+import { getErrorDetail } from "../../layouts/errorswitch";
 
 const PaymentWaitingTemplate = () => {
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [failmodalContent, setFailmodalContent] = useState("");
 
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -31,10 +36,24 @@ const PaymentWaitingTemplate = () => {
       console.log(data.data);
       navigate("/paymentresult", { state: { reservationData: data.data } });
     },
-    onError: (err) => {
-      console.error("error:", err);
+    onError: (error) => {
+      const errorDetail = getErrorDetail(error);
+      setFailmodalContent(errorDetail);
+      setIsModalOpen(true);
+      console.error("error:", error);
     },
   });
+  const handleConfirm = () => {
+    setIsModalOpen(false);
+    navigate("/");
+  };
+
+  const modalContent = (
+    <div className="flex flex-col gap-2">
+      <div> 오류가 발생하였습니다.</div>
+      <div>{failmodalContent}</div>
+    </div>
+  );
 
   const handlePayment = () => {
     const approvepostData = {
@@ -75,6 +94,14 @@ const PaymentWaitingTemplate = () => {
       >
         결제 완료를 위해 클릭하세요
       </Button>
+      <CustomModal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        onConfirm={handleConfirm}
+        title="오류발생"
+        content={modalContent}
+        confirmText="홈으로"
+      />
     </div>
   );
 };
